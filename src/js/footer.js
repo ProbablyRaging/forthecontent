4a1bf2dd26e497fbc0b6eedae172584ebd5d675a
Probably Raging <probablyraging@gmail.com>
2023-01-19 17:04:22 +1100

    Added onboarding page

--- src/js/footer.js
+++ src/js/footer.js
@@ -3,3 +3,13 @@ $('.burger').click(function () {
     $('html').toggleClass('mobile-nav--is-open');
     $('.nav__menu').toggleClass('nav__menu--hide').css({ display: "flex" });
 });
+
+$(function () {
+    $('#accordian h3').click(function () {
+        $(this).parent().parent().find('ul').slideUp(150);
+        if (!$(this).next().is(":visible")) {
+            $(this).next().slideDown(150);
+        }
+
+    });
+});
